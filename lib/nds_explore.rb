a71dda88d872cf47c610fe75c2f36c317144f083
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-07 20:32:01 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,12 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp nds
   nil
 end
 
+pretty_print_nds(directors_database)
+
 def print_first_directors_movie_titles
+  
 end
